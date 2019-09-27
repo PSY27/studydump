35618bc50c253df890118cfabf0694a0bd82723e
Utkarsh Bhardwaj <utbh23041999@gmail.com>
2019-09-28 04:48:26 +0530

    Renamed newline parameter for better readability

--- utils/DebugLogger/index.js
+++ utils/DebugLogger/index.js
@@ -13,33 +13,33 @@ const isDebugAllowed = () => {
 };
 
 // Show Error
-const error = (tip, msg, nl) => {
-  if (isDebugAllowed()) console.log('\n'.repeat(nl||0), '\x1b[31m', 'Error :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
+const error = (tip, msg, breaks) => {
+  if (isDebugAllowed()) console.log('\n'.repeat(breaks||0), '\x1b[31m', 'Error :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
 };
 
 // Show Success
-const success = (tip, msg, nl) => {
-  if (isDebugAllowed()) console.log('\n'.repeat(nl||0), '\x1b[32m', 'Success :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
+const success = (tip, msg, breaks) => {
+  if (isDebugAllowed()) console.log('\n'.repeat(breaks||0), '\x1b[32m', 'Success :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
 };
 
 // Show Warning
-const warn = (tip, msg, nl) => {
-  if (isDebugAllowed()) console.log('\n'.repeat(nl||0), '\x1b[33m', 'Warning :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
+const warn = (tip, msg, breaks) => {
+  if (isDebugAllowed()) console.log('\n'.repeat(breaks||0), '\x1b[33m', 'Warning :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
 };
 
-const debug = (tip, msg, nl) => {
-  if (isDebugAllowed()) console.log('\n'.repeat(nl||0), '\x1b[34m', 'Debug :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
+const debug = (tip, msg, breaks) => {
+  if (isDebugAllowed()) console.log('\n'.repeat(breaks||0), '\x1b[34m', 'Debug :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
 };
 
 // Show Info Tip
-const info = (tip, msg, nl) => {
-  if (isDebugAllowed()) console.log('\n'.repeat(nl||0), '\x1b[36m', 'Info :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
+const info = (tip, msg, breaks) => {
+  if (isDebugAllowed()) console.log('\n'.repeat(breaks||0), '\x1b[36m', 'Info :: ', tip, '\n', msg || '', '\n\r\x1b[0m');
 };
 
 // Show Waypoint Method
-const waypoint = (tip, methodArr, nl) => {
+const waypoint = (tip, methodArr, breaks) => {
   if (isDebugAllowed()) {
-    console.log('\n'.repeat(nl||0), '\x1b[34m', 'WayPoint :: ', tip, '\n\r\x1b[0m');
+    console.log('\n'.repeat(breaks||0), '\x1b[34m', 'WayPoint :: ', tip, '\n\r\x1b[0m');
     methodArr.map((method) => {
       console.log('\x1b[35m', '==>', method, '\n\r\x1b[0m');
       return true;
